bd270688e43aa13ec90b6d26152704d8292c3eb8
mma689 <lalkinslsth3@mail.ru>
2017-12-05 23:58:16 +0300

    Add recycle_view

--- app/src/main/java/project/lion/testingapp/ContactFragment.java
+++ app/src/main/java/project/lion/testingapp/ContactFragment.java
@@ -1,8 +1,9 @@
 package project.lion.testingapp;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,13 +14,38 @@ import android.view.ViewGroup;
 
 public class ContactFragment extends Fragment {
 
+    protected RecyclerView mRecyclerView;
+    protected CustomAdapter mAdapter;
+    protected RecyclerView.LayoutManager mLayoutManager;
+    protected String[] mDataset;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initDataset();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.contacts_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.recycler_view_fragment, container, false);
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mAdapter = new CustomAdapter(mDataset);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        return rootView;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
+
+    private void initDataset() {
+        mDataset = new String[60];
+        for (int i = 0; i < 60; i++) {
+            mDataset[i] = "This is element #" + i;
+        }
+    }
+
 }
